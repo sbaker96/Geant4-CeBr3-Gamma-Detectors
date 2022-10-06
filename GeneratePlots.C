3249fe718eccae495ed12869ae143da9293cb245
Scott Baker <sb20b@fsu.edu>
2022-10-06 16:45:22 -0400

    Added Comments

--- GeneratePlots.C
+++ GeneratePlots.C
@@ -23,7 +23,7 @@ const char* numAppend(const char* txt, int num);
 
 const char* srcName = "output.root";
 const char* outName = "Plots.root";
-const char* histName = "Edep";
+const char* histName = "Edep_";
 
 const int nofDetectors = 2;
 const int maxEnergy = 3000; // in keV
@@ -34,20 +34,23 @@ const int maxEnergy = 3000; // in keV
 
 int GeneratePlots()
 {
+	//Create Output File
 	TFile* outFile = new TFile(outName, "RECREATE");
 
 	outFile->Close();
-	
+
+	//Clear writeName
 	const char* writeName = numAppend("Flush", 0);
 	
+	//Generate raw and folded plots for each detector
 	for(int i = 0; i < nofDetectors; i++)
 	{
 		plotRaw(i);
 		plotFolded(i);
 	}
 
+	//Generate raw and folded 2D plots
 	plot2DRaw(0, 1);
-
 	plot2DFolded(0, 1);
 
 	return 0;
@@ -56,20 +59,29 @@ int GeneratePlots()
 
 void plotRaw(int num)
 {
+	//Start Message
+	cout << "Plotting Raw Data for Detector " << num << endl;
+	
+	//Open output from simulation
 	auto inFile = TFile::Open(srcName);
 	
+	//Create reader for the output ntuple
 	TTreeReader reader("Edep by Detectors", inFile);
 
+	//Get name of branch to look at
 	const char* branchName = numAppend(histName, num);
 
+	//Create readerValue for Edep
 	TTreeReaderValue<float> Edep(reader, branchName);
 
+	//Create Histogram
 	int nofBins = maxEnergy;
-			    
-	const char* writeName = numAppend("Raw", num);
+
+	const char* writeName = numAppend("Raw_", num);
 
 	TH1F* outHist = new TH1F(writeName, writeName, nofBins, 0, nofBins);
 
+	//Loop through values to fill histogram
 	while(reader.Next())
 	{
 		float edep = *Edep;
@@ -80,10 +92,12 @@ void plotRaw(int num)
 
 	}
 
+	//Set Histogram Option
 	outHist->SetOption("HIST");
 
 	outHist->SetLineColor(1);
 
+	//Write Histogram to output file
 	TFile* outFile = new TFile(outName, "UPDATE");
 
 	outHist->Write();
@@ -95,30 +109,41 @@ void plotRaw(int num)
 
 void plotFolded(int num)
 {
+	//Start Message 
+	cout << "Plotting Folded Data for Detector " << num << endl;
+        
+	//Open output from simulation
        	auto inFile = TFile::Open(srcName);
 
+	//Create reader for the output ntuple
         TTreeReader reader("Edep by Detectors", inFile);
 
+	//Get name of branch to look at
 	const char* branchName = numAppend(histName, num);
 
+	//Create readerValue for Edep
 	TTreeReaderValue<float> Edep(reader, branchName);
 
+	//Create Histogram
         int nofBins = maxEnergy;
 	
-	const char* writeName = numAppend("Fold", num);
+	const char* writeName = numAppend("Fold_", num);
 
         TH1F* outHist = new TH1F(writeName, writeName, nofBins, 0, nofBins);
 
+	//Create Functions
         TF1* stDev = new TF1("Standard Deviation", "(x/235.5)*(100/sqrt(x))", 0 , 1000*nofBins);
 
 	auto g = new TF1("g", "gausn(0)");
 
+	//Loop through values to fill histogram
 	while(reader.Next())
         {
                 float edep = *Edep;
 
                 edep *= 1000; //Convert Units
 	
+		//Set Parameters of gaussian
 		if( edep != 0)
 		{	
 		g->SetParameter(0, 1);
@@ -130,14 +155,17 @@ void plotFolded(int num)
 
         }
 
+	//Normalize Histogram
   	double factor = 1.0;
 
 	outHist->Scale(factor/outHist->GetMaximum());
 
+	//Set Histogram Option
 	outHist->SetOption("HIST");
 
         outHist->SetLineColor(1);
 
+	//Write Histogram to output file
 	TFile* outFile = new TFile(outName, "UPDATE");
 	
 	outHist->Write();
@@ -147,24 +175,30 @@ void plotFolded(int num)
 
 void plot2DRaw(int numA, int numB)
 {
+	//Start Message
 	cout << "Plotting 2D Raw" << endl;
 
+	//Open output from Simulation
 	auto inFile = TFile::Open(srcName);
 
+	//Create reader for the output ntuple
         TTreeReader reader("Edep by Detectors", inFile);
 
+	//Create readerValues for Edep
         TTreeReaderValue<float> EdepA(reader, numAppend(histName, numA));
         
 	TTreeReaderValue<float> EdepB(reader, numAppend(histName, numB));
 	
 	TTreeReaderValue<int> EventID(reader, "EventID");
 
+	//Create Histogram
         int nofBins = maxEnergy;
 
-        const char* writeName = numAppend(numAppend("Compare", numA), numB);
+        const char* writeName = numAppend(numAppend("Compare_", numA), numB);
 
 	TH2F* outHist = new TH2F(writeName, writeName, nofBins, 0, nofBins, nofBins, 0, nofBins);
 
+	//Create Set of Valid EventIDs
 	set<int> ids;
 
 	while(reader.Next())
@@ -173,6 +207,7 @@ void plot2DRaw(int numA, int numB)
 
 	}
 
+	//Loop through Event IDs
 	for(auto itr = ids.begin(); itr != ids.end(); itr++)
 	{
 //		cout << *itr << endl;
@@ -182,6 +217,7 @@ void plot2DRaw(int numA, int numB)
 	
 		reader.Restart();
 
+		//Loop through values to sum over energies for each event
 		while(reader.Next())
 		{
 
@@ -202,10 +238,12 @@ void plot2DRaw(int numA, int numB)
 	
 	}
 
+	//Set Histogram Options
 	outHist->SetOption("HIST");
 
         outHist->SetLineColor(1);
 
+	//Write Histogram to output file
         TFile* outFile = new TFile(outName, "UPDATE");
 
         outHist->Write();
@@ -218,28 +256,35 @@ void plot2DRaw(int numA, int numB)
 void plot2DFolded(int numA, int numB)
 {
 
+	//Start Message
 	cout << "Plotting 2D Folded" << endl;
 
+	//Open output from Simulation
 	auto inFile = TFile::Open(srcName);
 
+	//Create reader for the output ntuple
         TTreeReader reader("Edep by Detectors", inFile);
 
+	//Create readerValues for Edep
         TTreeReaderValue<float> EdepA(reader, numAppend(histName, numA));
         
 	TTreeReaderValue<float> EdepB(reader, numAppend(histName, numB));
 	
 	TTreeReaderValue<int> EventID(reader, "EventID");
 
+	//Create Histogram
         int nofBins = maxEnergy;
 
-        const char* writeName = numAppend(numAppend("CompareFold", numA), numB);
+        const char* writeName = numAppend(numAppend("CompareFold_", numA), numB);
 
 	TH2F* outHist = new TH2F(writeName, writeName, nofBins, 0, nofBins, nofBins, 0, nofBins);
 	
+	//Create Functions
 	TF1* stDev = new TF1("Standard Deviation", "(x/235.5)*(100/sqrt(x))", 0 , 1000*nofBins);
 		
 	TF2* g = new TF2("g", "[0]*exp(-(0.5*((x-[1])/[2])**2+(0.5*((y-[3])/[4])**2)))/(sqrt(2*pi)*[2]*[4])", 0, 20, 0, 20); //!!!
 
+	//Create Set of Valid EventIDs
 	set<int> ids;
 
 	while(reader.Next())
@@ -248,6 +293,7 @@ void plot2DFolded(int numA, int numB)
 
 	}
 
+	//Loop through Event IDs
 	for(auto itr = ids.begin(); itr != ids.end(); itr++)
 	{
 //		cout << *itr << endl;
@@ -257,6 +303,7 @@ void plot2DFolded(int numA, int numB)
 	
 		reader.Restart();
 
+		//Loop through values to sum over energies for each event
 		while(reader.Next())
 		{
 
@@ -273,6 +320,7 @@ void plot2DFolded(int numA, int numB)
         
         	edepB *= 1000;
 
+		//Set Parameters
 		if( edepA > 0 && edepB > 0)
                 {
                 g->SetParameter(0, 1);
@@ -290,14 +338,17 @@ void plot2DFolded(int numA, int numB)
 	
 	}
 
+	//Set Histogram Options
 	outHist->SetOption("HIST");
 
         outHist->SetLineColor(1);
 
+	//Normalize Histogram
 	double factor = 1.0;
 
         outHist->Scale(factor/outHist->GetMaximum());
 
+	//Write Histogram to output file
         TFile* outFile = new TFile(outName, "UPDATE");
         
 	outHist->Write();
@@ -307,12 +358,13 @@ void plot2DFolded(int numA, int numB)
 
 }
 
+//Function to append a number to a constant character array
 const char* numAppend(const char* txt, int num)
 {
 
 	stringstream ss;
 
-        ss << txt << "_"  << num;
+        ss << txt << num;
 	
 	const char* output = ss.str().c_str();
 
