db46282930b4dc906750fcd9fe243f0f4ea2418b
Scott Baker <sb20b@fsu.edu>
2022-10-07 13:06:22 -0400

    Added comments

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -56,15 +56,17 @@ void EventAction::EndOfEventAction(const G4Event* event)
 
 	G4int nofHits = trackerHC->entries();
 	
-	//Create List of TrackIDs corresponding to Decay Gammas
+	//Create Set  of TrackIDs corresponding to Decay Gammas
 
-	set<G4int> decayGammaIDs;
+	set<G4int> decayGammaIDs; //Using a set prevents the need to keep track of duplicates
 
+	//Loop through all Hits
 	for(G4int i = 0; i < nofHits; i++)
 	{
 		G4int currentSrc = (*trackerHC)[i]->GetDecayGammaSourceID();
 		if(currentSrc != -1)
 		{
+			//Check that the gamma itself deposited energy
 			G4bool isGamma = (*trackerHC)[i]->GetIsGamma();
 			if(isGamma)
 			{
@@ -79,32 +81,40 @@ void EventAction::EndOfEventAction(const G4Event* event)
 	G4int nofDetectors = 2;	
 	
 	//Fill Ntuple
+	
+	//Loop through Ids
 	for(auto itr = decayGammaIDs.begin(); itr != decayGammaIDs.end(); itr++)
 	{
 		G4int currentGammaID = *itr;
 	
+		//Loop through number of detectors
 		for(G4int n = 0; n < nofDetectors; n++)
 		{
 		
 			G4double totalEdep = 0.0;
 			
+			//Loop through hits
 			for(G4int i = 0; i < nofHits; i++)
                         {
 
-                                if((*trackerHC)[i]->GetDecayGammaSourceID() == currentGammaID
+                                //Add energy if hit matches current detector and decay ID
+				if((*trackerHC)[i]->GetDecayGammaSourceID() == currentGammaID
                                                 && (*trackerHC)[i]->GetDetectorNumber() == n)
                                 { totalEdep += (*trackerHC)[i]->GetEdep(); }
 
                         }
 
+			//Add total to ntuple
 			analysisManager->FillNtupleFColumn(n, totalEdep);
 
 		}
 
+		//Add eventID to ntuple
 		G4int eventID = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
 
 		analysisManager->FillNtupleIColumn(nofDetectors, eventID);
 
+		//Complete ntuple row
 		analysisManager->AddNtupleRow();
 	}
 }
