284d35e867b3363f1ade5dae5dcc5e3a55ec89d0
Scott Baker <sb20b@fsu.edu>
2023-07-20 16:02:40 -0400

    Implemented DetectorFront Class

---
+++ include/DetectorFront.hh
@@ -0,0 +1,77 @@
+//====================
+//DetectorFront Header
+//====================
+
+#ifndef DetectorFront_h
+#define DetectorFront_h
+
+#include "globals.hh"
+#include "G4LogicalVolume.hh"
+#include "G4AssemblyVolume.hh"
+#include "G4NistManger.hh"
+
+#include "DetectorCrystal.hh"
+
+namespace CeBr3
+{
+
+class DetectorFront
+{
+	public:
+		DetectorFront();
+		~DetectorFront();
+
+		//Set Functions
+		void SetRThickness(G4double r)  	{rThickness = r;};
+                void SetSThickness(G4double s)  	{sThickness = s;};
+                void SetGapFront(G4double g)    	{gapFront = g;};
+                void SetGapSide(G4double g)     	{gapSide = g;};
+
+                void SetRMat(G4Material* m)     	{rMat = m;};
+                void SetSMat(G4Material* m)     	{sMat = m;};
+
+		void SetCrystal(DetectorCrystal* c )	{crystal = c;};
+
+		//Get Functions
+		G4double GetRThickness()        	{return rThickness;};
+                G4double GetSThickness()        	{return sThickness;};
+                G4double GetGapFront()          	{return gapFront;};
+                G4double GetGapSide()           	{return gapSide;};
+
+		G4Material* GetRMat()			{return rMat;};
+		G4Material* GetSMat()			{return sMat;};
+
+		DetectorCrystal* GetCrystal()		{return crystal;};
+
+		//Construct Detector Front
+		void ConstructDetFront();
+
+		
+	
+	private:
+		//Parameters
+		G4double rThickness = 0;
+                G4double sThickness = 0;
+                G4double gapFront = 0;
+                G4double gapSide = 0;
+                G4Material* rMat;
+                G4Material* sMat;
+
+		//Crystal
+		DetectorCrystal* crystal;
+
+		//Assembly Volume
+		G4AssemblyVolume* detFront;
+
+		//AuxillaryFunctions
+		G4LogicalVolume* GenerateCan(G4double thick, G4double inRad, 
+				G4double length, G4Material* mat, G4String name);
+
+
+
+
+}
+
+}
+
+#endif
---
+++ src/DetectorFront.cc
@@ -0,0 +1,116 @@
+//============================
+//DetectorFront Implementation
+//============================
+
+#include "DetectorFront.hh"
+#include "G4Tubs.hh"
+#include "G4SubtractionSolid.hh"
+
+namespace CeBr3
+{
+
+DetectorFront::DetectorFront()
+{;}
+
+DetectorFront::~DetectorFront()
+{;}
+
+void DetectorFront::ConstructDetFront()
+{
+	//Parameters
+	G4RotationMatrix* nullRot =  new G4RotationMatrix();
+
+	G4double c_hz = crystal->GetLength()/2;
+	G4double c_outRad = crystal->GetWidth()/2;
+
+	G4double c_x = 0*mm;
+	G4double c_y = 0*mm;
+	G4double c_x = 0*mm;
+
+	//Generate Reflector
+	
+	G4double rInRad = c_outRad;
+	G4double rLength = rThickness + 2*c_hz;
+	
+	G4LogicalVolume* rLog = GenerateCan(rThickness, rInRad, rLength, rMat, "Reflector");
+
+	G4double r_x = c_x;
+	G4double r_y = c_y;
+	G4double r_z = c_z - (rThickness/2);
+
+	//Generate Shell
+	
+	G4double sInRad = c_outRad + gapSide;
+	G4double sLength = sThickness + gapFront + 2*c_hz;
+
+	G4LogicalVolume* sLog = GenerateCan(sThickness, sInRad, sLength, sMat, "Shell");
+
+	G4double s_x = c_x;
+	G4double s_y = c_y;
+	G4double s_z = c_z - (sThickness + gapFront)/2;
+
+	//Generate Assembly
+	
+	detFront = new DetectorAssembly;
+	
+	//Rotation and Translation Matrices
+        G4RotationMatrix Ra;
+        G4ThreeVector Ta;
+
+        G4Transform3D Ma;
+
+        //Add Crystal
+        Ta.setX(c_x); Ta.setY(c_y); Ta.setZ(c_z);
+        Ma = G4Transform3D(Ra, Ta);
+        detFront->AddPlacedVolume(crystal, Ma);
+
+        //Add Reflector
+        Ta.setX(r_x); Ta.setY(r_y); Ta.setZ(r_z);
+        Ma = G4Transform3D(Ra, Ta);
+        detFront->AddPlacedVolume(rLog, Ma);
+
+        //Add Shell
+        Ta.setX(s_x); Ta.setY(s_y); Ta.setZ(s_z);
+        Ma = G4Transform3D(Ra, Ta);
+        detFront->AddPlacedVolume(sLog, Ma);
+	
+	
+}
+
+G4LogicalVolume* DetectorFront::GenerateCan(G4double thick, G4double inRad,
+                                G4double length, G4Material* mat, G4String name)
+{
+	G4RotationMatrix* nullRot = new G4RotationMatrix();
+	
+	//Base Parameters
+	G4double bInRad = 0.0*mm
+	G4double bOutRad = inRad + thick;
+	G4double b_hz = length/2;
+
+	//Base Solid
+	G4Tubs* baseSolid = new G4Tubs("baseSolid", bInRad, bOutRad, b_hz, 0*deg, 360*deg);
+	
+	//Hole Parameters
+	G4double hInRad = 0.0*mm;
+	G4double hOutRad = inRad,
+	G4double h_hz = length/2;
+
+	//Hole Solid
+	G4Tubs* holeSolid = new G4Tubs("holeSolid", hInRad, hOutRad, h_hz, 0*deg, 360*deg);
+
+	//Hole Positioning
+	G4ThreeVector holeTrans = new G4ThreeVector(0*mm, 0*mm, 1*thick);
+
+	//Build Solid
+	G4SubtractionSolid* canSolid = new G4SubtractionSolid("Can Solid", baseSolid, baseHole, holeTrans, nullRot);
+
+	//Build Logical Volume
+	G4LogicalVolume* canLog = new G4LogicalVolume(canSolid, mat, name);
+
+	return canLog;
+
+	
+}
+
+
+}
