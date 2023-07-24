c4b7da9c3d666cbaf1e701bb671f7affbdb4415f
Scott Baker <sb20b@fsu.edu>
2023-07-24 14:03:13 -0400

    Added detBack and  width/length functions

--- include/DetectorAssembly.hh
+++ include/DetectorAssembly.hh
@@ -6,6 +6,7 @@
 #define DetectorAssembly_h
 
 #include "DetectorFront.hh"
+#include "DetectorBack.hh"
 #include "globals.hh"
 #include "G4AssemblyVolume.hh"
 
@@ -21,19 +22,27 @@ class DetectorAssembly
 
 		//Set Functions
 		void SetDetFront(DetectorFront* f)	{detFront = f;};
+		void SetDetBack(DetectorBack* b)	{detBack = b;};
 
 		//Get Functions
 		DetectorFront* GetDetFront()		{return detFront;};
+		DetectorBack* GetDetBack()		{return detBack;};
 
 		void ConstructAssembly();
 
 		void PlaceAssembly(G4LogicalVolume* worldLog, G4double dist, G4double gap,
                         G4double theta, G4double phi, G4double spin);
 
+		//Helper Functions
+		G4double GetLength();
+
+		G4double GetWidth();
 		
 	private:
 		//Components
 		DetectorFront* detFront;
+
+		DetectorBack* detBack;
 
 		G4AssemblyVolume* assembly;
 
