db64c23456c417ee73c9009d0a2cc83c647840e3
Scott Baker <sb20b@fsu.edu>
2023-07-26 09:59:34 -0400

    added cathode parameters to header

--- include/DetectorPMT.hh
+++ include/DetectorPMT.hh
@@ -23,17 +23,21 @@ class DetectorPMT
 		void SetWidth(G4double w)		{width = w;};
 		void SetLength(G4double l)		{length = l;};
 		void SetThickness(G4double t)		{thickness = t;};
+		void SetCathodeThick(G4double t)	{cathodeThick = t;};
 
 		void SetGlassMat(G4Material* m)		{glassMat = m;};
 		void SetGasMat(G4Material* m)		{gasMat = m;};
+		void SetCathodeMat(G4Material* m)	{cathodeMat = m;};
 
 		//Get Functions
 		G4double GetWidth()			{return width;};
 		G4double GetLength()			{return length;};
 		G4double GetThickness()			{return thickness;};
+		G4double GetCathodeThick()		{return cathodeThick;};
 
 		G4Material* GetGlassMat()		{return glassMat;};
 		G4Material* GetGasMat()			{return gasMat;};
+		G4Material* GetCathodeMat()		{return cathodeMat;};
 
 		G4AssemblyVolume* GetPMT()		{return pmt;};
 
@@ -45,9 +49,11 @@ class DetectorPMT
 		G4double width;
 		G4double length;
 		G4double thickness;
+		G4double cathodeThick;
 
 		G4Material* glassMat;
 		G4Material* gasMat;
+		G4Material* cathodeMat;
 
 		//Assembly
 		G4AssemblyVolume* pmt;
