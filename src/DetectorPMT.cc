88a5895386e620d1474f88278e1134059a1ecc35
Scott Baker <sb20b@fsu.edu>
2023-07-27 09:14:51 -0400

    Corrected gas length and placement

--- src/DetectorPMT.cc
+++ src/DetectorPMT.cc
@@ -31,19 +31,21 @@ void DetectorPMT::ConstructPMT()
 	G4double cathode_hz = cathodeThick/2;
 	G4double cathode_x = glass_x;
 	G4double cathode_y = glass_y;
-	G4double cathode_z = glass_z + (glass_hz - thickness - cathode_hz);
+	G4double cathode_z = glass_z - (glass_hz - thickness - cathode_hz);
 
 	G4double gasOutRad = glassOutRad - thickness;
 	G4double gas_hz = glass_hz - cathode_hz - thickness;
 	G4double gas_x = glass_x;
 	G4double gas_y = glass_y;
-	G4double gas_z = glass_z - (cathode_hz);
+	G4double gas_z = glass_z + (cathode_hz);
 
 
 	//Solid Construction
 	G4Tubs* outerSolid = new G4Tubs("outerSolid", inRad, glassOutRad, glass_hz, 0*deg, 360*deg);
+	G4Tubs* innerSolid = new G4Tubs("innerSolid", inRad, gasOutRad, gas_hz + cathode_hz, 0*deg, 360*deg);
 	G4Tubs* cathodeSolid = new G4Tubs("cathodeSolid", inRad, cathodeOutRad, cathode_hz, 0*deg, 360*deg);
-	G4Tubs* innerSolid = new G4Tubs("innerSolid", inRad, gasOutRad, gas_hz, 0*deg, 360*deg);
+	G4Tubs* gasSolid = new G4Tubs("gasSolid", inRad, gasOutRad, gas_hz, 0*deg, 360*deg);
+
 
 	G4SubtractionSolid* shellSolid = new G4SubtractionSolid("shellSolid", outerSolid, innerSolid);
 
@@ -51,7 +53,7 @@ void DetectorPMT::ConstructPMT()
 	
 	G4LogicalVolume* glassLog = new G4LogicalVolume(shellSolid, glassMat, "PMT Glass");
 	G4LogicalVolume* cathodeLog = new G4LogicalVolume(cathodeSolid, cathodeMat, "PMT Cathode");
-	G4LogicalVolume* gasLog = new G4LogicalVolume(innerSolid, gasMat, "PMT Gas"); 
+	G4LogicalVolume* gasLog = new G4LogicalVolume(gasSolid, gasMat, "PMT Gas"); 
 
 	//Assembly Construction
 	
@@ -60,17 +62,18 @@ void DetectorPMT::ConstructPMT()
 	//Rotation and Translation Matrices
         G4RotationMatrix Ra;
         G4ThreeVector Ta;
+	G4Transform3D Ma;
 
 	Ta.setX(glass_x); Ta.setY(glass_y); Ta.setZ(glass_z);
-        G4Transform3D Ma =  G4Transform3D(Ra, Ta);
+        Ma =  G4Transform3D(Ra, Ta);
 	pmt->AddPlacedVolume(glassLog, Ma);
 	
 	Ta.setX(cathode_x); Ta.setY(cathode_y); Ta.setZ(cathode_z);
-        G4Transform3D Ma =  G4Transform3D(Ra, Ta);
+        Ma =  G4Transform3D(Ra, Ta);
 	pmt->AddPlacedVolume(cathodeLog, Ma);
 	
 	Ta.setX(gas_x); Ta.setY(gas_y); Ta.setZ(gas_z);
-        G4Transform3D Ma =  G4Transform3D(Ra, Ta);
+        Ma =  G4Transform3D(Ra, Ta);
 	pmt->AddPlacedVolume(gasLog, Ma);
 	
 }
