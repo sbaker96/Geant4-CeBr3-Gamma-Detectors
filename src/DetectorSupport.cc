bda1361c69475c3f2ac7555f312345d454611262
Scott Baker <sb20b@fsu.edu>
2024-03-07 13:34:41 -0500

    Added detector support implimentation

---
+++ src/DetectorSupport.cc
@@ -0,0 +1,35 @@
+//================================
+// Detector Support Implementation
+//================================
+
+#include "DetectorSupport.hh"
+#include "G4SystemOfUnits.hh"
+
+namespace CeBr3
+{
+
+	DetectorSupport::DetectorSupport()
+	{;}
+
+	DetectorSupport::~DetectorSupport()
+	{;}
+
+	G4Transform3D DetectorSupport::GenerateShift()
+	{
+		//Shift Coordinates
+		G4double s_x = 0.0*cm;
+		G4double s_y = height/2;
+		G4double s_z = 0.0*cm;
+		
+		//Rotation and Translation Matrices
+       		G4RotationMatrix Ra;
+        	G4ThreeVector Ta;
+		
+        	G4Transform3D Ma;
+		
+		Ta.setX(s_x); Ta.setY(s_y); Ta.setZ(s_z);
+		Ma = G4Transform3D(Ra, Ta);
+
+	}
+
+}
