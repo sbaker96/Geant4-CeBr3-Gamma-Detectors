7ee7e3926dea25245fbd00d8e89e51dd27d0c5eb
Scott Baker <sb20b@fsu.edu>
2024-03-07 13:24:27 -0500

    Added detector support header

---
+++ include/DetectorSupport.hh
@@ -0,0 +1,42 @@
+//========================
+// Detector Support Header
+//========================
+
+#ifndef DetectorSupport_h
+#define DetectorSupport_h
+
+#include "globals.hh"
+#include "G4NistManager.hh"
+
+namespace CeBr3
+{
+
+class DetectorSupport
+{
+	public: 
+		DetectorSupport();
+		~DetectorSupport();
+
+		//Set Functions
+		void setHeight(G4double h)		{height = h;};
+
+		void setMat(G4Material* m)		{mat = m;};
+
+		//Get Functions
+		G4double getHeight()			{return height;};
+
+		G4Material* getMat()			{return mat;};
+
+		//Helper Functions
+		G4Transform3D GenerateShift();
+		
+	private:
+		//Parameters
+		G4double height;
+		
+		G4Material* mat;	
+}
+
+}
+
+#endif
